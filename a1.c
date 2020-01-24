5e8d5b8bc085de61563e3e9cafd4f7b029ac5854
sai774 <60249606+sai774@users.noreply.github.com>
2020-01-24 10:54:12 +0530

    Add files via upload

---
+++ a1.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+struct student
+{
+ char name[50];
+ int age;
+ float marks;
+ }s1;
+ union student1
+{
+ char name[50];
+ int age;
+ float marks;
+}s2;
+ void main()
+{
+ printf("size of structure variable is %d\n",sizeof(s1));
+ printf("size of union variable is %d\n",sizeof(s2));
+}
